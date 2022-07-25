d341e8851d6884727617b12f35d20b2d1e32664f
drJedi <47307348+drJedi@users.noreply.github.com>
2022-07-25 17:02:09 +0200

    admin login admin form

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: "mongodb+srv://drJedix:34Rb4XdbhW.!eCR@cluster0.gxdgu.mongodb.net/?retryWrites=true&w=majority",
+  db: "e",
   keySession: ["TWOJKLUCZ"],
   maxAgeSession: 24 * 60 * 60 * 1000,
 };
