52e67063d25997facd82b592787ab640df845976
drJedi <47307348+drJedi@users.noreply.github.com>
2022-07-26 12:48:59 +0200

    update form with vote nad percents

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: "e",
+  db: "mongodb+srv://drJedix:34Rb4XdbhW.!eCR@cluster0.gxdgu.mongodb.net/?retryWrites=true&w=majority",
   keySession: ["TWOJKLUCZ"],
   maxAgeSession: 24 * 60 * 60 * 1000,
 };
